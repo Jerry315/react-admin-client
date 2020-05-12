33f0a499ec277d37987bc561882141c17dfffaf6
Jerry315 <710899905@qq.com>
2020-05-12 08:20:51 +0800

    新增login组件样式

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,13 +1,20 @@
 import React,{Component} from "react";
 import './login.less'
+import logo from './images/logo.png'
 
 // 登录路由组件
 export default class Login extends Component{
     render() {
         return (
             <div className="login">
-                <header className="login-header"></header>
-                <section className="login-content"></section>
+                <header className="login-header">
+                    <img src={logo} alt="logo"/>
+                    <h1>React项目：后台管理系统</h1>
+                </header>
+                <section className="login-content">
+                    <h2>用户登录</h2>
+                    <div>Form组件标签</div>
+                </section>
             </div>
         )
     }
